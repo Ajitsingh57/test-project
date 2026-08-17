7ac1f10de53253780872cf24418ad1af8a93f060
Ajit Singh <ajitsingh.aks27@gmail.com>
2026-08-17 19:21:41 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { connectDB } from './config/db.js';
-import dns from 'node:dns';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import dns from "node:dns";
 import path from "path";
-import userRouter from './routes/userRoute.js';
-import watchRouter from './routes/watchRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
 
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-const app=express();
-const port =5000;
+import userRouter from "./routes/userRoute.js";
+import watchRouter from "./routes/watchRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// MongoDB DNS issue ke liye
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-//MIDDLEWARES
+app.use(express.urlencoded({ extended: true }));
 
-//DB
+// DB
 connectDB();
 
-//ROUTES
-app.use("/api/auth",userRouter);
-app.use("/uploads",express.static(path.join(process.cwd(),"uploads")));
-app.use("/api/watches",watchRouter);
-app.use('/api/cart',cartRouter);
-app.use('/api/orders',orderRouter);  
+// ROUTES
+app.use("/api/auth", userRouter);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/api/watches", watchRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/orders", orderRouter);
 
-app.get('/',(req,res)=>{
-    res.send("API working");
+app.get("/", (req, res) => {
+  res.send("API working");
 });
 
-app.listen(port,()=>{
-    console.log(`Server started on port ${port}`);
-})
+// Vercel ke liye app export
+export default app;
